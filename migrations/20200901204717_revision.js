70ec2cf1ca7087b2127a91f13858553bbf91f7f9
donovanrichardson <donovanspqa@gmail.com>
2020-09-01 21:42:56 -0400

    fixed migration

--- migrations/20200901204717_revision.js
+++ migrations/20200901204717_revision.js
@@ -4,6 +4,7 @@ exports.up = function(knex) {
         t.bigInteger("id").primary()
         t.integer("article_id")
         t.bigInteger("timestamp")
+        t.integer("diff")
 
         t.foreign('article_id').references("id").inTable("article").onDelete("CASCADE").onUpdate("CASCADE");
     })
